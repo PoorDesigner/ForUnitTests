f7db97a619b80965a28df2723fa3e07b0ebcd99c
chait <chait@DESKTOP-OAG0BNK>
2016-08-29 23:17:59 +0530

    Add project files.

---
+++ ForTestingFrameWork.Tests/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.QualityTools.Testing.Fakes;
+using System.Collections.Generic;
+using Logging;
+
+namespace ForTestingFrameWork.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (ShimsContext.Create())
+            {
+                var expectedResult = new List<Album>();
+                // Arrange:
+                // Detour DateTime.Now to return a fixed date:
+                Logging.Fakes.ShimMusic.AllInstances.GetAlbumsByArtistString =
+                (customer, options) =>
+                {
+                    expectedResult.AddRange(this.GetOtherAlbums());
+                    return expectedResult;
+                };
+                ForTestingFrameWork.Fakes.ShimService1.AllInstances.GetOtherAlbums =
+                (result) =>
+                {
+                    return this.GetOtherAlbums();
+                    
+                };
+                // Instantiate the component under test:
+                var componentUnderTest = new Service1();
+
+                // Act:
+                var actualResult = componentUnderTest.GetAlbumsByArtist("artist");
+
+                // Assert: 
+                // This will always be true if the component is working:
+                CollectionAssert.AreEqual(expectedResult, actualResult);
+            }
+        }
+
+        private List<Album> GetOtherAlbums()
+        {
+            Random ran = new Random();
+            return new List<Album>()
+            {
+                new Album()
+                {
+                    AlbumID = ran.Next(),
+                    AlbumName = ran.NextDouble().ToString(),
+                    Artist = ran.NextDouble().ToString()
+                }
+            };
+        }
+    }
+}
---
+++ ForTestingFrameWork/Service1.svc.cs
@@ -0,0 +1,37 @@
+﻿using Logging;
+using Misc;
+using System;
+using System.Collections.Generic;
+using Microsoft.Practices.Unity;
+
+namespace ForTestingFrameWork
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public List<Album> GetAlbumsByArtist(string name)
+        {
+            var Container = new UnityContainer();
+            Container.RegisterType<IMusic, Music>(new ContainerControlledLifetimeManager());
+            var Depe = Container.Resolve<IMusic>();
+            var albums = Depe.GetAlbumsByArtist(name);
+            var logsFromOtherApp = GetOtherAlbums();
+            albums.AddRange(logsFromOtherApp);
+            return albums;
+        }
+
+        private List<Album> GetOtherAlbums()
+        {
+            return new List<Album>()
+            {
+                new Album()
+                {
+                    AlbumID = 2,
+                    AlbumName = "Album2",
+                    Artist = "Test"
+                }
+            };
+        }
+    }
+}
